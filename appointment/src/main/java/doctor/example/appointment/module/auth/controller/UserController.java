e62e6bb76787199e4fc3567948e7f6e1c5646403
Anuruddh <anuruddh209401@gmail.com>
2026-04-11 16:06:02 +0530

    Api tested user completed

--- appointment/src/main/java/doctor/example/appointment/module/auth/controller/UserController.java
+++ appointment/src/main/java/doctor/example/appointment/module/auth/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pharmacy")
 public class UserController {
@@ -24,7 +26,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public Users login(@RequestBody Users user){
+    public Map<String, Object> login(@RequestBody Users user){
         return userService.login(user);
     }
 }
