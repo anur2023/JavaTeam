903b1764d0c87b9734885e6c4ae04e008a4f0546
Yash Uppal <2k22.cse.2213230@gmail.com>
2026-04-13 13:41:02 +0530

    backend structure edit

---
+++ OnlineStationary/OnlineStationary/src/test/java/com/example/OnlineStationary/OnlineStationaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineStationary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStationaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
